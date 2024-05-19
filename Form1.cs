caa14cf1e2aa0020bc17fe7be98eef5d05b79cea
EgorSborschikov <162421841+EgorSborschikov@users.noreply.github.com>
2024-05-19 16:02:21 +0700

    add new methods to code & debug

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,19 @@ namespace AttestetionWork
 {
     public partial class Form1 : Form
     {
+        private double[] dataX;
+        private double[] dataY;
+        private string series_name_data_default = "Данные";
+        private string series_name_data;
+
+        private double[] dataXe;
+
+        private double[] data_line;
+        private double coef_line_a, coef_line_b;
+        private string series_name_line = "Линейная";
+
+        private double stepX;
+
         public Form1()
         {
             InitializeComponent();
@@ -66,8 +79,8 @@ namespace AttestetionWork
             {
                 MessageBox.Show("Число элементов Оси Абцисс и оси Ординат не сошлось", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            //dataX = new double[CountX];
-            //dataY = new double[CountY];
+            dataX = new double[CountX];
+            dataY = new double[CountY];
 
             OpenFileDialog openDialog = new OpenFileDialog();
             openDialog.Filter = "Файл Excel|*.XLSX;*.XLS";
@@ -77,7 +90,7 @@ namespace AttestetionWork
                 MessageBox.Show("Файл не выбран!", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            //string fileName = System.IO.Path.GetFileName(openDialog.FileName);
+            string fileName = System.IO.Path.GetFileName(openDialog.FileName);
 
             Microsoft.Office.Interop.Excel.Application ExcelApp;
             Microsoft.Office.Interop.Excel.Workbook ExcelWorkbook;
@@ -108,7 +121,7 @@ namespace AttestetionWork
                     for (int i = RangeCb - 1; i < RangeCe; i++)
                     {
                         int j = RangeRb;
-                        //dataX[p++] = Convert.ToDouble(ExcelWorksheet.Cells[j, i + 1].Text.ToString());
+                        dataX[p++] = Convert.ToDouble(ExcelWorksheet.Cells[j, i + 1].Text.ToString());
                     }
                 }
                 else//если данные хранятся в файле по столбцам
@@ -117,12 +130,12 @@ namespace AttestetionWork
                     for (int j = RangeRb - 1; j < RangeRe; j++)
                     {
                         int i = RangeCb;
-                        //dataX[p++] = Convert.ToDouble(ExcelWorksheet.Cells[j + 1, i].Text.ToString());
+                        dataX[p++] = Convert.ToDouble(ExcelWorksheet.Cells[j + 1, i].Text.ToString());
                     }
                 }
 
                 //Загрузка Y
-                /*if (RangeDByRows)//если данные хранятся в файле в виде строки
+                if (RangeDByRows)//если данные хранятся в файле в виде строки
                 {
                     int p = 0;
                     for (int i = RangeDCb - 1; i < RangeDCe; i++)
@@ -130,7 +143,7 @@ namespace AttestetionWork
                         int j = RangeDRb;
                         dataY[p++] = Convert.ToDouble(ExcelWorksheet.Cells[j, i + 1].Text.ToString());
                     }
-                    if (checkBoxCaptionY.Checked && RangeDCb > 1)//Загрузка подписи данных
+                    if (checkBox1.Checked && RangeDCb > 1)//Загрузка подписи данных
                         series_name_data = ExcelWorksheet.Cells[RangeDRb, RangeDCb - 1].Text.ToString();
                     else
                         series_name_data = series_name_data_default;
@@ -143,11 +156,11 @@ namespace AttestetionWork
                         int i = RangeDCb;
                         dataY[p++] = Convert.ToDouble(ExcelWorksheet.Cells[j + 1, i].Text.ToString());
                     }
-                    if (checkBoxCaptionY.Checked && RangeDRb > 1)//Загрузка подписи данных
+                    if (checkBox1.Checked && RangeDRb > 1)//Загрузка подписи данных
                         series_name_data = ExcelWorksheet.Cells[RangeDRb - 1, RangeDCb].Text.ToString();
                     else
                         series_name_data = series_name_data_default;
-                }*/
+                }
             }
             catch
             {
@@ -158,37 +171,198 @@ namespace AttestetionWork
             GC.Collect();   //Инициировать сборщик мусора
 
             //Вычисление среднего шага оси Х
-            /*stepX = (dataX.Last() - dataX.First()) / (dataX.Length - 1);
+            stepX = (dataX.Last() - dataX.First()) / (dataX.Length - 1);
 
             //Вычисления, построение графиков
             calculate();
             set_chart_series();
-            checkBoxSeries_CheckedChanged(null, null);// Проверка выводимых линий трендов
+            ToggleLinearRegressionLine(true);// Проверка выводимых линий трендов
 
             update_labels_coef();
 
             update_predict_table();
-            update_acorrel_table();*/
+            
         }
 
         private void numericUpDownPredictNum_ValueChanged(object sender, EventArgs e)
         {
             //Вычисления и построение графиков
-            //calculate();
-            //set_chart_series();
+            calculate();
+            set_chart_series();
         }
 
-        /*private void update_labels_coef()
+        private void update_labels_coef()
         {
-            labelCoefLine.Text = "y = " + coef_line_a.ToString("N") + " + " + coef_line_b.ToString("N") + " * x";
+            label10.Text = "y = " + coef_line_a.ToString("N") + " + " + coef_line_b.ToString("N") + " * x";//вид регрессии
             
             //параметры линейной функции
-            labelRxyLine.Text = (coef_line_b * Math.Sqrt(sigma2(dataX)) / Math.Sqrt(sigma2(dataY))).ToString("N");
-            labelAperLine.Text = (approx_error(dataY, data_line)).ToString("N") + " %";
-            labelR2Line.Text = (1.0 - sigma2(data_line) / sigma2(dataY)).ToString("N");
+            label11.Text = (coef_line_b * Math.Sqrt(sigma2(dataX)) / Math.Sqrt(sigma2(dataY))).ToString("N");//расчёт Rxy
+            label12.Text = (approx_error(dataY, data_line)).ToString("N") + " %";// расчёт A
+            label13.Text = (1.0 - sigma2(data_line) / sigma2(dataY)).ToString("N");// расчёт R2
 
             //Коэффициент корреляции
-            labelCorrel.Text = correlation(dataX, dataY).ToString("N");
-        }*/
+            label14.Text = correlation(dataX, dataY).ToString("N");
+        }
+
+
+        private void update_predict_table()
+        {       
+            System.Data.DataTable dt = new System.Data.DataTable();
+            dt.Columns.Add("сдвиг");
+            dt.Columns.Add("Год");
+            dt.Columns.Add("Линейная функция");
+
+            int offset = -2;
+            double year = dataX[dataX.Length + offset];
+            for (int i = 0; i < 20; i++)
+            {
+                double new_year = year + stepX * i;
+                DataRow r = dt.NewRow();
+                r["сдвиг"] = offset + i;
+                r["Год"] = new_year;
+                r["Линейная функция"] = f_line(new_year).ToString("N");
+                dt.Rows.Add(r);
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        private void ToggleLinearRegressionLine(bool showLine)
+        {
+            //серия данных для линейной регрессии имеет имя "Линейная регрессия"
+            chart1.Series["Линейная регрессия"].Enabled = showLine;
+        }
+
+        private void set_chart_series()
+        {
+            chart1.Series.Clear();
+
+            // Настройка осей
+            chart1.ChartAreas[0].AxisX.Minimum = dataX.Min();
+            chart1.ChartAreas[0].AxisX.Maximum = dataX.Max() + stepX * (double)numericUpDown2.Value;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = stepX;
+
+            // Добавление серии данных для точек из файла
+            chart1.Series.Add("Данные");
+            chart1.Series["Данные"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            chart1.Series["Данные"].ChartArea = "ChartArea1";
+            chart1.Series["Данные"].Points.DataBindXY(dataX, dataY);
+
+            // Добавление серии данных для линейной регрессии
+            chart1.Series.Add("Линейная регрессия");
+            chart1.Series["Линейная регрессия"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["Линейная регрессия"].ChartArea = "ChartArea1";
+
+            // Расчет линейной регрессии и привязка данных к серии
+            double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+            int n = dataX.Length;
+            for (int i = 0; i < n; i++)
+            {
+                sumX += dataX[i];
+                sumY += dataY[i];
+                sumXY += dataX[i] * dataY[i];
+                sumX2 += dataX[i] * dataX[i];
+            }
+            double b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+            double a = (sumY - b * sumX) / n;
+
+            double minX = dataX.Min();
+            double maxX = dataX.Max() + stepX * (double)numericUpDown2.Value;
+            for (double x = minX; x <= maxX; x += stepX)
+            {
+                double y = a + b * x;
+                chart1.Series["Линейная регрессия"].Points.AddXY(x, y);
+            }
+        }
+
+        private double f_line(double x)
+        {
+            return coef_line_a + coef_line_b * x;
+        }
+
+        private void make_dataXe(int predict_num)
+        {
+            dataXe = new double[dataX.Length + predict_num];
+            for (int i = 0; i < dataXe.Length; i++)
+            {
+                if (i < dataX.Length)
+                    dataXe[i] = dataX[i];
+                else
+                    dataXe[i] = dataXe[i - 1] + stepX;
+            }
+        }
+
+        private void calculate()
+        {
+            make_dataXe((int)numericUpDown2.Value);
+
+            calc_line();
+        }
+
+        private double mean(double[] x)
+        {
+            double res = 0;
+            int n = x.Length;
+            for (int i = 0; i < n; i++)
+            {
+                res += x[i];
+            }
+            return res / n;
+        }
+
+        private double sigma2(double[] x)
+        {
+            double res = 0;
+            double meanx = mean(x);
+            int n = x.Length;
+            for (int i = 0; i < n; i++)
+            {
+                double tmp = (x[i] - meanx);
+                res += tmp * tmp;
+            }
+            return res / n;
+        }
+
+        private double mean_product(double[] x, double[] y)
+        {
+            double res = 0;
+            int n = x.Length;
+            for (int i = 0; i < n; i++)
+            {
+                res += x[i] * y[i];
+            }
+            return res / n;
+        }
+
+        private void calc_line()
+        {
+            coef_line_b = (mean_product(dataX, dataY) - mean(dataX) * mean(dataY)) / sigma2(dataX);
+            coef_line_a = mean(dataY) - coef_line_b * mean(dataX);
+            data_line = new double[dataXe.Length];
+            for (int i = 0; i < data_line.Length; i++)
+            {
+                data_line[i] = f_line(dataXe[i]);
+            }
+        }
+        private double approx_error(double[] Y, double[] f)
+        {
+            double res = 0;
+            int n = Y.Length;
+            for (int i = 0; i < n; i++)
+            {
+                res += Math.Abs((Y[i] - f[i]) / Y[i]);
+            }
+            return res / n * 100;
+        }
+
+        private double correlation(double[] X, double[] Y)
+        {
+            double res = 0;
+            int n = X.Length;
+            for (int i = 0; i < n; i++)
+            {
+                res += (X[i] - mean(X)) * (Y[i] - mean(Y));
+            }
+            return res / (Math.Sqrt(sigma2(X)) * Math.Sqrt(sigma2(Y)) * n);
+        }
     }
 }
